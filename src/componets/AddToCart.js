23a555c9a4a13285b5c565dd4d9f149af56c1409
Anshu123Chauhan <anshu.rani@ens.enterprises>
2025-10-16 15:31:37 +0530

    price issue on cart

--- src/componets/AddToCart.js
+++ src/componets/AddToCart.js
@@ -265,7 +265,7 @@ export default function AddToCart() {
                         ₹{(p.price * p.quantity).toFixed(0)}
                       </div>
                       <div className="text-sm line-through text-gray-400">
-                        ₹{p.mrp}
+                        {p?.variant?.mrp || p?.mrp? "₹"+p?.variant?.mrp || "₹"+p?.mrp :""}
                       </div>
                     </div>
                   </div>
