e5ecaf2f21a71cb4bafb331a5f4a56c46426dfd6
Anshu123Chauhan <anshu.rani@ens.enterprises>
2025-10-16 12:56:53 +0530

    content changed

--- src/componets/OffersBanner.js
+++ src/componets/OffersBanner.js
@@ -3,19 +3,19 @@ import React, { useState, useEffect } from "react";
 export default function OffersBanner() {
     const offers = [
         {
-            text: "🔥 Flat 50% OFF on New Arrivals",
+            text: "🔥 Discover the Latest Trends — Just Arrived!",
             img: "https://images.unsplash.com/photo-1607083206869-4c7672e72a8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            text: "👜 Buy 1 Get 1 Free on Handbags",
+            text: "👜 Elevate Your Style with Our Exclusive Collections",
             img: "https://images.unsplash.com/photo-1572584642822-6f8de0243c93?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            text: "👟 Extra 20% OFF on Footwear",
+            text: "👟 Designed for Comfort. Made to Move.",
             img: "https://plus.unsplash.com/premium_photo-1676166012743-aee27f3415a7?q=80&w=1367&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            text: "🎁 Free Shipping on Orders Above ₹999",
+            text: "🎁 Seamless Shopping, Stunning Styles – Only Here",
             img: "https://images.unsplash.com/photo-1607083206869-4c7672e72a8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
     ];
